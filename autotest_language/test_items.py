1ec010f6a840146c39db437bf20469934e05cc6b
stas khass <stas.khass@test.ru>
2023-04-08 17:32:50 +0300

    Добавили чтобы тест не создавал критическую ошибку при ненахождение элемента

--- autotest_language/test_items.py
+++ autotest_language/test_items.py
@@ -1,10 +1,16 @@
-
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 import time
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_check_button_cart(browser):
     browser.get(link)
     time.sleep(30)
-    cart_button = browser.find_element(By.CSS_SELECTOR, "div.content div#content_inner div.row div.product_main > form button.btn-add-to-basket")
-    assert cart_button != None , "Ошибка: Кнопка не существует"
+
+    try:
+        cart_button = browser.find_element(By.CSS_SELECTOR, "div.content div#content_inner div.row div.product_main > form button.btn-add-to-basket")
+    except NoSuchElementException:
+        assert False , "Error: NoSuchElement"
+    else:
+        assert True
