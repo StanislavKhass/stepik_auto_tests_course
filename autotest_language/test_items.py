3c05b7e2527103a6d7afcb5c508264f10aa1fdbd
stas khass <stas.khass@test.ru>
2023-04-08 16:35:58 +0300

    поменял время sleep

--- autotest_language/test_items.py
+++ autotest_language/test_items.py
@@ -6,5 +6,5 @@ from selenium.webdriver.common.by import By
 def test_check_button_cart(browser):
     browser.get(link)
     cart_button = browser.find_element(By.CSS_SELECTOR, "div.content div#content_inner div.row div.product_main form button")
-    time.sleep(10)
+    time.sleep(30)
     assert cart_button , "Ошибка: Кнопка не существует"
