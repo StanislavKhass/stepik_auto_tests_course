23baee70274ba8d2cd1ceba27d98eec9f80472ea
stas khass <stas.khass@test.ru>
2023-04-08 16:30:04 +0300

    добавил проверку метод для выбоки селекторов

--- autotest_language/test_items.py
+++ autotest_language/test_items.py
@@ -1,6 +1,10 @@
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+import time
+from selenium.webdriver.common.by import By
 
 def test_check_button_cart(browser):
     browser.get(link)
     cart_button = browser.find_element(By.CSS_SELECTOR, "div.content div#content_inner div.row div.product_main form button")
+    time.sleep(10)
     assert cart_button , "Ошибка: Кнопка не существует"
